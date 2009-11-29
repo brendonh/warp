bd55932e5ba3dafee2544d41544cc71b3ad0a3f8
Brendon Hogger <brendonh@gmail.com>
2009-11-29 18:08:50 +0800

    Render a page!!

--- warp/webserver/resource.py
+++ warp/webserver/resource.py
@@ -2,6 +2,8 @@ from zope.interface import implements
 
 from twisted.web.resource import IResource, NoResource
 
+from mako.template import Template
+
 from warp.webserver import auth, node
 from warp.runtime import config
 
@@ -61,28 +63,41 @@ class WarpResource(object):
             
     def render(self, request):
         self.args = request.postpath
-        print self
 
         if not self.facetName:
             request.redirect(request.childLink('index'))
             return "Redirecting..."
 
-        if request.avatar is None:
-            loggedIn = "Not logged in."
-        else:
-            loggedIn = "Logged in as %s" % request.avatar.email.encode("utf-8")
+        templatePath = (config['siteDir']
+                        .child('nodes')
+                        .child(self.nodeName)
+                        .child(self.facetName + ".mak"))
 
-        return """
-%s<br />
-<form method="POST" action="/__login__">
-Email: <input type="text" name="email" /><br />
-Pass: <input type="text" name="password" /><br />
-<input type="submit" value="Log in" />
-</form>
-""" % loggedIn
+        template = Template(filename=templatePath.path)
+
+        return template.render()
 
 
-    def __repr__(self):
-        return "<NodeResource: %s::%s (%s)>" % (
-            self.nodeName, self.facetName, self.args)
+
+
+
+
+#         if request.avatar is None:
+#             loggedIn = "Not logged in."
+#         else:
+#             loggedIn = "Logged in as %s" % request.avatar.email.encode("utf-8")
+
+#         return """
+# %s<br />
+# <form method="POST" action="/__login__">
+# Email: <input type="text" name="email" /><br />
+# Pass: <input type="text" name="password" /><br />
+# <input type="submit" value="Log in" />
+# </form>
+# """ % loggedIn
+
+
+#     def __repr__(self):
+#         return "<NodeResource: %s::%s (%s)>" % (
+#             self.nodeName, self.facetName, self.args)
 
