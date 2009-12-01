6b318c9b72cb0be34db3e23944e12a161aa6545b
brendonh <brendonh@gmail.com>
2009-12-01 19:48:22 +0800

    Take a siteDir option, and use that

---
+++ warp/iwarp.py
@@ -0,0 +1,9 @@
+from zope.interface import Interface
+
+
+class IWarpService(Interface):
+    """
+    Implemented by the Warp service plugin, to allow easy startup
+    from other plugins
+    """
+    pass
