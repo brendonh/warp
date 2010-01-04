9ac489fb7f68436db920e0ab597b4e8016cbbfec
Brendon Hogger <brendonh@gmail.com>
2010-01-05 00:19:32 +0800

    Okay how about like this

--- setup.py
+++ setup.py
@@ -21,6 +21,11 @@ setup(name="warp",
         "pytz",
         ],
 
+      packages = [
+        "warp",
+        "twisted.plugins"
+        ],
+
       package_data = {
         "twisted": ['plugins/warp_plugin.py'],
         }
