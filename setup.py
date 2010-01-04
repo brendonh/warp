90e5a127d20454429b3ac963f132e83a46227cd7
Brendon Hogger <brendonh@gmail.com>
2010-01-04 23:52:18 +0800

    Update setup stuff

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ setup(name="warp",
       version="0.0.1",
       packages = find_packages(),
       zip_safe = False,
+      include_package_data=True,
 
       description="Easy-to-use layer over twisted.web",
       author="Brendon Hogger",
       author_email="brendonh@taizilla.com",
       url="http://wiki.github.com/brendonh/warp",
       long_description=open('README').read(),
+
+      download_url="http://github.com/brendonh/warp/tarball/master#egg=warp",
 )
