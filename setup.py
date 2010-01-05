e112f28193b69c087917d0de54f22a20160c2e7c
Brendon Hogger <brendonh@gmail.com>
2010-01-05 21:06:19 +0800

    Seriously, don't even look

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup, find_packages
 
-print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-print "Hi I'm warp's setup.py"
-print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-
-
 setup(name="warp",
       version="0.0.1",
       zip_safe = False,
@@ -37,3 +28,12 @@ setup(name="warp",
         }
 
 )
+
+
+def cleanupTwistedCache():
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    print "Haha cache cleanup"
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+import atexit
+atexit.register(cleanupTwistedCache)
