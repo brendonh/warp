b80acd4f68686cd9c665af1ea35494a592a3e329
Brendon Hogger <brendonh@gmail.com>
2012-09-17 20:10:07 +0800

    Simple event system for Storm models

---
+++ warp/common/events.py
@@ -0,0 +1,72 @@
+import traceback
+from collections import defaultdict
+
+from storm.locals import Store, Storm
+from storm.info import get_obj_info
+
+
+handlers = defaultdict(list)
+
+def handler(event, *models):
+    if not models:
+        models = [None]
+    else:
+        models = [model.__name__ for model in models]
+
+    def decorate(fun):
+        for modelName in models:
+            handlers[(modelName, event)].append(fun)
+        return fun
+
+    return decorate
+
+
+
+class CommitEventStore(Store):
+
+    def __init__(self, database, cache=None):
+        self.events = []
+        super(CommitEventStore, self).__init__(database, cache)
+
+
+    def commit(self):
+        super(CommitEventStore, self).commit()
+
+        # Event handlers can emit new events, which will run after
+        # all existing events are handled. Do a little dance here
+        # to make this clean.
+        while self.events:
+            events = self.events
+            self.events = []
+            for event in events:
+                event.run()
+
+
+                
+class EventModel(Storm):
+    
+    def emit(self, event):
+        store = get_obj_info(self)["store"]
+        if store is None:
+            raise Exception("Tried to emit event for store-less object")
+
+        store.events.append(PendingEvent(self, event))
+
+
+
+class PendingEvent(object):
+    def __init__(self, obj, event):
+        self.obj = obj
+        self.event = event
+
+    def run(self):
+        modelName = self.obj.__class__.__name__
+        eventHandlers = (set(handlers.get((modelName, self.event), [])) 
+                         | set(handlers.get((None, self.event), [])))
+
+        for handler in eventHandlers:
+            try:
+                handler(self.obj)
+            except Exception:
+                print ">>> Error in event handler"
+                traceback.print_exc()
